274c309853967fe561e33433312c8bc0093167d6
dmitiry_vanin <van777dim@gmail.com>
2018-07-07 22:17:07 +0300

    test

--- src/test/java/com/rtb/Settings/Base.java
+++ src/test/java/com/rtb/Settings/Base.java
@@ -4,23 +4,35 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.concurrent.TimeUnit;
 
 public class Base {
 
    protected WebDriver driver;
+   public static WebDriver chromedriver;
 
     @Before
     public void setUp() {
-        driver = new ChromeDriver();
-        driver.get("https://relaxtheback-com.myshopify.com");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        WebDriver chromedriver =new ChromeDriver(capabilities);
+        chromedriver.get("https://relaxtheback-com.myshopify.com");
+
+//      driver = new ChromeDriver();
+//      driver.get("https://relaxtheback-com.myshopify.com");
+        chromedriver.manage().window().maximize();
+        chromedriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    @After
-    public void tearDown(){
-        driver.close();
-    }
+//    @After
+//    public void tearDown(){
+//        chromedriver.close();
+//    }
 }
--- src/test/java/com/rtb/Tests/LoginTest.java
+++ src/test/java/com/rtb/Tests/LoginTest.java
@@ -1,6 +1,7 @@
 package com.rtb.Tests;
 
 import com.rtb.Pages.HomePage;
+import com.rtb.Pages.VisitPage;
 import com.rtb.Settings.Base;
 import org.junit.Test;
 
@@ -9,6 +10,8 @@ public class LoginTest extends Base {
     //login
     @Test
     public void login() throws InterruptedException {
+        VisitPage visitpage = new VisitPage(driver);
+        visitpage.enterStore();
         HomePage homepage = new HomePage(driver);
         homepage.clickSignIn();
     }
